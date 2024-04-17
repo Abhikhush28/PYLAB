7726867b80c815e6212a5b746fb28d15b2e323ea
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-17 09:20:10 +0530

    code

---
+++ DAY-6/anagram.py
@@ -0,0 +1,22 @@
+# Write a function isAnagrams () to validate if the provided two strings are anagrams or not.
+ 
+# If  the two strings are anagrams, then return ‘Anagrams'.
+ 
+# Otherwise, return ‘Word is not anagrams'.
+ 
+# Words Example: race and care, listen and silent
+
+
+def isAnagrams(str1, str2):
+ if sorted(str1) == sorted(str2):
+  return True
+ else:
+  return False
+
+str1 = input("Enter the str1: ")
+str2 = input("Enter the str2: ")
+
+if isAnagrams(str1, str2):
+ print("Anagrams")
+else:
+ print("Not Anagrams")
