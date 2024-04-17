a18f437737c10407e82c7f30931fd531b078c7f5
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-17 16:56:15 +0530

    new code added

--- DAY-6/task.py
+++ DAY-6/task.py
@@ -4,11 +4,8 @@
  
 # max is   66
 
-
 import sys
 n = len(sys.argv)
-print("Total arguments passed:", n)
-print("\nArguments passed:", end = " ")
 num = []
 for i in range(1, n):
    num.append(int(sys.argv[i]))
