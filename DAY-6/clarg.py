087daa73c792f43ece03e4d19972a79e1ee39438
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-17 10:58:44 +0530

    code added

--- DAY-6/clarg.py
+++ DAY-6/clarg.py
@@ -10,5 +10,8 @@ print("\nArguments passed:", end = " ")
 num = []
 for i in range(1, n):
    num.append(int(sys.argv[i]))
-print("sum: ", sum(num))
-print("Average: ", sum(num)/len(num))
+try:
+ print("sum: ", sum(num))
+ print("Average: ", sum(num)/len(num))
+except ZeroDivisionError:
+ print("Error: divison by not zero ", )
