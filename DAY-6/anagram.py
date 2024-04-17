55ec557daaf1796e69cf2b3fec9b6b5516222db2
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-17 09:27:16 +0530

    new code  updated

--- DAY-6/anagram.py
+++ DAY-6/anagram.py
@@ -7,7 +7,32 @@
 # Words Example: race and care, listen and silent
 
 
+# [9:19 AM] N Murugadoss (Unverified)
+# def Anagrams(s1,s2):
+
+#     s1=s1.replace('','').lower()
+
+#     s2=s2.replace('','').lower()
+
+#     if len(s1)!=len(s2):
+
+#         return 'Word is not anagrams'
+
+#     if sorted(s1) == sorted(s2):
+
+#         return 'Anagrams'
+
+#     else:
+
+#         return 'Word is not anagrams'
+ 
+# Anagrams('race','care')
+ 
+ 
+
 def isAnagrams(str1, str2):
+ str1 = str1.strip().lower()
+ str2 = str2.strip().lower()
  if sorted(str1) == sorted(str2):
   return True
  else:
