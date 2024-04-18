cbdff0ce2d9e5ec26179a5524cc3faa22840d850
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-18 10:40:14 +0530

    changes pushed

--- Day-7/solution.py
+++ Day-7/solution.py
@@ -4,5 +4,9 @@ y = int(input("Enter value for y: "))
 
 
 sum = calulation.add(x,y)
+sub = calulation.sub(x, y)
+prod = calulation.prod(x, y)
 
-print(sum)
+print("sum: ",sum)
+print("diff: ", sub)
+print("prod: ", prod)
