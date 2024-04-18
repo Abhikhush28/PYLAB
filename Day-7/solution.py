c9343fbf1d598d9af5606a2fc522d8c22b37d135
Abhikhush28 <sahuabhikhush@gmail.com>
2024-04-18 10:43:56 +0530

    new code added

--- Day-7/solution.py
+++ Day-7/solution.py
@@ -1,12 +1,13 @@
-import calulation
+from calulation import add
 x = int(input("Enter value for x: "))
 y = int(input("Enter value for y: "))
 
 
-sum = calulation.add(x,y)
-sub = calulation.sub(x, y)
-prod = calulation.prod(x, y)
+# sum = calulationadd(x,y)
+sum = add(x,y)
+# sub = calulation.sub(x, y)
+# prod = calulation.prod(x, y)
 
 print("sum: ",sum)
-print("diff: ", sub)
-print("prod: ", prod)
+# print("diff: ", sub)
+# print("prod: ", prod)
